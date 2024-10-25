39b95e27a16e8b6461d04fad5a561b7dd6ec6844
CarsonDev1 <buitritinht@gmail.com>
2024-10-25 12:44:00 +0700

    upload code

--- service/vnpay.service.js
+++ service/vnpay.service.js
@@ -7,7 +7,7 @@ export const createVnpayPayment = async (orderId, amount, orderInfo, bankCode = 
 		const { vnp_TmnCode, vnp_HashSecret, vnp_Url, vnp_ReturnUrl } = vnpayConfig;
 
 		const vnp_Version = '2.1.0';
-		const ipAddr = '192.168.111.102';
+		const ipAddr = '0.0.0.0';
 		const vnp_Command = 'pay';
 		const vnp_CurrCode = 'VND';
 		const locale = 'vn';
